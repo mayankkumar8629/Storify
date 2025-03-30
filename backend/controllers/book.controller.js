0d1b9925951a4735fcd76687e3c4b0d1a6034a09
Mayank Kumar <mayankkumarverma306@gmail.com>
2025-03-30 17:19:12 +0530

    added update,delete and createNew routes

--- backend/controllers/book.controller.js
+++ backend/controllers/book.controller.js
@@ -11,3 +11,103 @@ export const getAllBooks = async(req,res)=>{
         res.status(500).json({message:"Error in getting books",error});
     }
 }
+
+export const getBookById = async(req,res)=>{
+    try{
+        const {id}=req.params;
+        const book=await Book.findById(id);
+        if(!book){
+            res.status(404).json({message:"Book does not exists"});
+        }
+        res.status(200).json(book);
+    }catch(error){
+        res.status(500).json({message:"Error in finding book",error});
+    }
+}
+
+//update by id
+export const updateBookById = async(req,res)=>{
+    try{
+        const {
+            title,
+            description,
+            authors,
+            price,
+            imageUrl,
+            stock,
+            categories
+        }=req.body;
+        
+        const updatedData = {};
+        const {id} = req.params;
+        if(title!==undefined) updatedData.title=title;
+        if(description!==undefined) updatedData.description=description;
+        if(authors!==undefined)updatedData.authors=authors;
+        if(price!==undefined)updatedData.price=price;
+        if(imageUrl!==undefined)updatedData.imageUrl=imageUrl;
+        if(stock!==undefined)updatedData.stock=stock;
+        if(categories!==undefined)updatedData.categories=categories;
+
+        const updatedBook = await Book.findByIdAndUpdate(
+            id,
+            updatedData,
+            {new:true}
+        );
+        if(!updatedBook){
+            res.status(404).json({message:"Cannot find Book"});
+        }
+        res.status(200).json({
+            message:"Book updated succesfuly",
+            data:updatedBook
+        });
+
+        
+    }catch(error){
+        res.status(500).json({message:"Error updating book"});
+    }
+}
+
+//delete by id
+export const deleteById = async (req,res)=>{
+    try{
+        const {id}=req.params;
+        const deletedBook= await Book.findByIdAndDelete(id);
+        if(!deletedBook){
+            return res.status(404).json({message:"Book not found"});
+        }
+        res.status(200).json({message:"Book delete successfully"});
+    }catch(error){
+        res.status(500).json({message:"Error in deleting book",error});
+    }
+};
+//creating a new book
+export const createNewBook = async(req,res)=>{
+    try{
+        const{
+            title,
+            description,
+            authors,
+            price,
+            imageUrl,
+            publicationDate,
+            stock,
+            categories
+        }= req.body;
+        //creating new book
+        const newBook = new Book({
+            title,
+            description,
+            authors,
+            price,
+            imageUrl,
+            publicationDate,
+            stock,
+            categories
+        });
+        await newBook.save();
+        res.status(201).json({messge:"Book added successfully",newBook});
+
+    }catch(error){
+        res.status(500).json({messge:"eror adding new book entry",error});
+    }
+};
--- backend/routes/book.route.js
+++ backend/routes/book.route.js
@@ -1,8 +1,12 @@
 import express from "express";
-import { getAllBooks } from "../controllers/book.controller.js";
+import { getAllBooks ,getBookById,updateBookById,deleteById,createNewBook} from "../controllers/book.controller.js";
 
 const router=express.Router();
 
 router.get("/getAll",getAllBooks);
+router.get("/getAll/:id",getBookById);
+router.put("/getAll/:id/update",updateBookById);
+router.delete("/getAll/:id/delete",deleteById);
+router.post("/createNew",createNewBook);
 
 export default router;
